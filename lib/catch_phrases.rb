d9712d4da27bd4c49f1c648d1b8a365a06448da3
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-09-05 17:18:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,3 +2,8 @@
       phrase = "It's-a me, Mario!"
       puts phrase
     end
+    
+    def toadstool
+      status = "Thank you Mario! But our princess is in another castle!"
+      puts status
+    end
